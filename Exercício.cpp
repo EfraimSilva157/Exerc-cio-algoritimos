9e1a944ea7444015a658ff8473d913bed752f54b
Efraim Pereira <efraimpereira116@gmail.com>
2026-04-15 23:31:21 -0300

    exercício.cpp

--- Exercício.cpp
+++ Exercício.cpp
@@ -1,60 +1,77 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
+
+float calcularMedia(float n1, float n2, float n3) {
+    return (n1 + n2 + n3) / 3;
+}
+
 int main() {
-    string nome[5];
-    float n1[5], n2[5], n3[5], media[5];
+    string nomes[5];
+    float notas[5][3];
+    float medias[5];
 
     
     for (int i = 0; i < 5; i++) {
-        cout << "Aluno " << i + 1 << endl;
+        cout << "\nAluno " << i + 1 << endl;
 
         cout << "Nome: ";
-        cin >> nome[i];
+        cin >> nomes[i];
 
-        cout << "Nota 1: ";
-        cin >> n1[i];
+        for (int j = 0; j < 3; j++) {
+            cout << "Nota " << j + 1 << ": ";
+            cin >> notas[i][j];
+        }
 
-        cout << "Nota 2: ";
-        cin >> n2[i];
-
-        cout << "Nota 3: ";
-        cin >> n3[i];
-
-        media[i] = (n1[i] + n2[i] + n3[i]) / 3;
-
-        cout << endl;
+        medias[i] = calcularMedia(notas[i][0], notas[i][1], notas[i][2]);
     }
 
     
     int maior = 0;
-
     for (int i = 1; i < 5; i++) {
-        if (media[i] > media[maior]) {
+        if (medias[i] > medias[maior]) {
             maior = i;
         }
     }
 
     
-    cout << "\nRESULTADOS:\n\n";
+    cout << "\n\n===== TABELA =====\n\n";
+
+    cout << left
+         << setw(15) << "Nome"
+         << setw(10) << "N1"
+         << setw(10) << "N2"
+         << setw(10) << "N3"
+         << setw(10) << "Media"
+         << setw(15) << "Situacao"
+         << endl;
+
+    cout << "-------------------------------------------------------------\n";
+
+    cout << fixed << setprecision(2);
 
     for (int i = 0; i < 5; i++) {
-        cout << "Nome: " << nome[i] << endl;
-        cout << "Notas: " << n1[i] << ", " << n2[i] << ", " << n3[i] << endl;
-        cout << "Media: " << media[i] << endl;
+        string situacao;
 
-        if (media[i] >= 7) {
-            cout << "Situacao: Aprovado" << endl;
-        } else {
-            cout << "Situacao: Reprovado" << endl;
-        }
+        if (medias[i] >= 7)
+            situacao = "Aprovado";
+        else
+            situacao = "Reprovado";
 
-        cout << "---------------------\n";
+        cout << left
+             << setw(15) << nomes[i]
+             << setw(10) << notas[i][0]
+             << setw(10) << notas[i][1]
+             << setw(10) << notas[i][2]
+             << setw(10) << medias[i]
+             << setw(15) << situacao
+             << endl;
     }
 
     
-    cout << "\nMelhor aluno: " << nome[maior];
-    cout << " com media " << media[maior] << endl;
+    cout << "\nMelhor aluno: " << nomes[maior]
+         << " (Media: " << medias[maior] << ")" << endl;
 
     return 0;
 }
